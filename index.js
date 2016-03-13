c33b5cf6c1abc1e5a9a5b728ed35df22bff21401
TheConnMan <bcconn2112@gmail.com>
2016-03-13 12:17:14 -0400

    Add initial API endpoint

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/api/slack', function(req, res) {
+	try {
+		var result = {
+			"ok": true
+		};
+		if (req.query.apiKey !== process.env.API_KEY) {
+			throw "Invalid API key";
+		}
+		if (!req.query.text) {
+			throw "Requires a text parameter";
+		}
+		res.json(result);
+	} catch (e) {
+		console.log(e);
+		res.json({
+			"ok": false,
+			"error": e
+		});
+	}
+});
+
 var server = app.listen(3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
